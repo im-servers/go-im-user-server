f4c8e0f20d98a48d9b497570986d4c36ef610e3e
heyehang <heyehang>
2022-11-05 17:24:29 +0800

    get device -> pipeliner

--- internal/logic/getdevicetokensbyuseridlogic.go
+++ internal/logic/getdevicetokensbyuseridlogic.go
@@ -7,10 +7,11 @@ import (
 	"go-im-user-server/internal/svc"
 
 	"github.com/heyehang/go-im-grpc/user_server"
-	"github.com/pkg/errors"
 
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/stores/redis"
+
+	goredis "github.com/go-redis/redis/v8"
 )
 
 type GetDeviceTokensByUserIDLogic struct {
@@ -33,15 +34,25 @@ func NewGetDeviceTokensByUserIDLogic(ctx context.Context, svcCtx *svc.ServiceCon
 
 func (l *GetDeviceTokensByUserIDLogic) GetDeviceTokensByUserID(in *user_server.GetDeviceTokensByUserIDReq) (*user_server.GetDeviceTokensByUserIDReply, error) {
 	var userDeviceTokenMap = make(map[int64]*user_server.ListDeviceToken, 0)
-	for _, v := range in.Ids {
-		deviceTokens, err := l.store.SmembersCtx(l.ctx, fmt.Sprintf("%s%v", cacheGoImServerUserDeviceTokenPrefix, v))
+
+	cmdRes := make([]*goredis.StringSliceCmd, 0, len(in.Ids))
+	err := l.store.PipelinedCtx(l.ctx, func(p redis.Pipeliner) error {
+		for _, v := range in.Ids {
+			cmdRes = append(cmdRes, p.SMembers(l.ctx, fmt.Sprintf("%s%v", cacheGoImServerUserDeviceTokenPrefix, v)))
+		}
+		return nil
+	})
+	if err != nil {
+		l.Logger.Error(err)
+		return &user_server.GetDeviceTokensByUserIDReply{}, err
+	}
+	for i, v := range cmdRes {
+		deviceTokens, err := v.Result()
 		if err != nil {
-			err = errors.WithMessage(err, "GetSetCtx err")
 			l.Logger.Error(err)
 			return &user_server.GetDeviceTokensByUserIDReply{}, err
 		}
-		//l.Logger.Slowf("%v:%s", v, deviceTokens)
-		userDeviceTokenMap[v] = &user_server.ListDeviceToken{Values: deviceTokens}
+		userDeviceTokenMap[in.Ids[i]] = &user_server.ListDeviceToken{Values: deviceTokens}
 	}
 
 	return &user_server.GetDeviceTokensByUserIDReply{UserDeviceTokens: userDeviceTokenMap}, nil
